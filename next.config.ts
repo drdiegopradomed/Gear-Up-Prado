5117613cdb187d8a117227e4b9690fa6ef3bcdd8
drdiegopradomed <drdiegopradomed@gmail.com>
2026-06-07 11:15:28 -0300

    chore: add Shopee CDN to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.aliexpress.com",
       },
+      {
+        protocol: "https",
+        hostname: "down-br.img.susercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.susercontent.com",
+      },
     ],
   },
 };
